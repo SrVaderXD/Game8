fc2954147bbbc8e35ba74ec0a9e34d37bc2bb023
Henrique Loureiro de Faria <henriquelfaria08@gmail.com>
2021-06-23 16:14:54 -0300

    Crab sprites and animation, now they disappear when intersected with the hole in the middle of the map and now they follow a path to the center

--- src/entity/Crab.java
+++ src/entity/Crab.java
@@ -1,26 +1,60 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import main.Game;
 
 public class Crab {
 
 	public double x, y, dx, dy;
 	public double speed = 4;
 
+	public static BufferedImage[] crabSprites;
+	private int curIndex = 0, maxIndex = 3, curAnimationFrames = 0, maxAnimationFrames = 15;
+
 	public Crab(int x, int y) {
 		this.x = x;
 		this.y = y;
+		double radius = Math.atan2((Game.HEIGHT / 2 - 20) - y, (Game.WIDTH / 2 - 20) - x);
+		this.dx = Math.cos(radius);
+		this.dy = Math.sin(radius);
+		crabSprites = new BufferedImage[3];
+
+		for (int i = 0; i < crabSprites.length; i++) {
+			crabSprites[i] = Game.spritesheet.getSprite(i * 16, 0);
+		}
 	}
 
 	public void tick() {
-		x++;
-		y++;
+		x += (dx * speed);
+		y += (dy * speed);
+
+		if (new Rectangle((int) x, (int) y, 40, 40).intersects(Game.holeMask)) {
+			Game.crabs.remove(this);
+			return;
+		}
+
+		animation();
+
 	}
 
 	public void render(Graphics g) {
-		g.setColor(Color.red);
-		g.fillRect((int) x, (int) y, 40, 40);
+		g.drawImage(crabSprites[curIndex], (int) x, (int) y, 40, 40, null);
+	}
+
+	private void animation() {
+		curAnimationFrames++;
+
+		if (curAnimationFrames >= maxAnimationFrames) {
+			curAnimationFrames = 0;
+			curIndex++;
+
+			if (curIndex >= maxIndex) {
+				curIndex = 0;
+			}
+		}
 	}
 
 }
--- src/main/Game.java
+++ src/main/Game.java
@@ -4,6 +4,7 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferStrategy;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +19,18 @@ public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	public static int WIDTH = 480;
 	public static int HEIGHT = 480;
-
+	public static Spritesheet spritesheet;
 	public static List<Crab> crabs;
 	private Spawner spawner;
+	public static Rectangle holeMask;
 
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		spritesheet = new Spritesheet("/spritesheet.png");
 		crabs = new ArrayList<>();
-
 		crabs.add(new Crab(16, 16));
-
 		spawner = new Spawner();
+		holeMask = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 10, 10);
 	}
 
 	public void tick() {
@@ -50,9 +52,9 @@ public class Game extends Canvas implements Runnable {
 		g.setColor(new Color(252, 210, 136));
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		g.setColor(new Color(217, 161, 100));
-		g.fillOval(WIDTH / 2 - 35, HEIGHT / 2 - 35, 70, 70);
+		g.fillOval(WIDTH / 2 - 20, HEIGHT / 2 - 20, 40, 40);
 		g.setColor(new Color(193, 140, 86));
-		g.fillOval(WIDTH / 2 - 25, HEIGHT / 2 - 25, 50, 50);
+		g.fillOval(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
 
 		for (int i = 0; i < crabs.size(); i++) {
 			crabs.get(i).render(g);
